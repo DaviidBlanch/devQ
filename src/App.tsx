4eafd16e7f78a5ddec3f723a878db85b68191532
David Blanch Bellido <dablabe98@gmail.com>
2023-07-29 23:49:42 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Title from './components/Title'
+
+function App() {
+  return (
+    <main className='m-auto max-w-4xl'>
+      <Title />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Title.tsx
@@ -0,0 +1,13 @@
+export default function Title() {
+    return (
+        <div>
+
+            <h1 className="font-black text-6xl text-center pt-8">
+                devQ
+            </h1>
+            <h5 className="block py-1 text-center ">
+                Página web creada por David Blanch
+            </h5>
+        </div>
+    )
+}
