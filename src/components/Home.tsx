0fa07e5ec87b09024572b81159df0c021384556a
David Blanch Bellido <dablabe98@gmail.com>
2023-07-31 18:50:03 +0200

    update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,9 +3,9 @@ import Questions from "./Questions";
 
 export default function Home() {
     return (
-        <div className="relative overflow-y-scroll h-[calc(100vh-20px)] p-4 pb-0">
+        <div className="relative overflow-y-scroll h-[calc(100vh-20px)] p-0 pb-0">
             <NavHome />
-            <main>
+            <main className="p-4">
                 <Questions />
             </main>
         </div>
--- src/components/NavHome.tsx
+++ src/components/NavHome.tsx
@@ -2,8 +2,8 @@ import { UserProfileIcon } from "../assets/Icons";
 
 export default function NavHome() {
     return (
-        <nav className=" w-full z-20 top-0 left-0 rounded-lg bg-transparent mb-2 pb-2">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto pl-0">
+        <nav className="sticky w-full z-20 top-0 left-0 p-4 pb-1 bg-transparent">
+            <div className=" max-w-screen-xl flex flex-wrap items-center justify-between mx-auto pl-0">
                 <span className="self-center text-2xl font-semibold whitespace-nowrap">devQ</span>
                 <UserProfileIcon />
             </div>
