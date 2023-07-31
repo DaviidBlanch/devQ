08fcc79765c5fd07a94e593820c45ecd2c091918
David Blanch Bellido <dablabe98@gmail.com>
2023-07-31 19:10:13 +0200

    Scroll hidden

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@ import Questions from "./Questions";
 
 export default function Home() {
     return (
-        <div className="relative overflow-y-scroll h-[calc(100vh-20px)] p-0 pb-0">
+        <div className="relativen overflow-y-scroll will-change-scroll h-[calc(100vh-20px)] p-0 pb-0">
             <NavHome />
             <main className="p-4">
                 <Questions />
--- src/components/NavHome.tsx
+++ src/components/NavHome.tsx
@@ -2,7 +2,7 @@ import { UserProfileIcon } from "../assets/Icons";
 
 export default function NavHome() {
     return (
-        <nav className="sticky w-full z-20 top-0 left-0 p-4 pb-1 bg-transparent">
+        <nav className="sticky w-auto z-20 top-0 left-0 p-4 pb-1 bg-transparent">
             <div className=" max-w-screen-xl flex flex-wrap items-center justify-between mx-auto pl-0">
                 <span className="self-center text-2xl font-semibold whitespace-nowrap">devQ</span>
                 <UserProfileIcon />
