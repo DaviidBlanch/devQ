ab74966b400c046229115461347418652b2332fc
David Blanch Bellido <dablabe98@gmail.com>
2023-08-01 17:59:06 +0200

    Navbar done

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import NavHome from "./NavHome";
 import Questions from "./Questions";
 
 export default function Home() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    const handleScroll = (event: React.UIEvent<HTMLDivElement>) => {
+        setIsScrolled(event.currentTarget.scrollTop > 0);
+    };
+
     return (
-        <div className="relative overflow-y-scroll will-change-scroll h-[calc(100vh-20px)] p-0 pb-0">
-            <NavHome />
+        <div
+            className="relative overflow-y-scroll will-change-scroll h-[calc(100vh-20px)] p-0 pb-0 "
+            onScroll={handleScroll}
+        >
+            <NavHome scroll={isScrolled} />
             <main className="p-4 pt-3">
                 <Questions />
             </main>
-        </div>
+        </div >
     )
 }
--- src/components/NavHome.tsx
+++ src/components/NavHome.tsx
@@ -1,10 +1,16 @@
+import React from "react";
 import { UserProfileIcon } from "../assets/Icons";
 
-export default function NavHome() {
+interface Scroll {
+    scroll: boolean;
+}
+
+const NavHome: React.FC<Scroll> = ({ scroll }) => {
+    console.log(scroll)
     return (
         <nav
             id="nav-home"
-            className="sticky w-auto z-20 top-0 left-0 px-4 py-3 rounded-t-lg transition-colors duration-100"
+            className={`sticky w-auto z-20 top-0 left-0 px-4 py-3 rounded-t-lg transition-colors: duration-1000 ${scroll ? "bg-[#0d363f]" : "bg-transparent"}`}
         >
             <div className=" max-w-screen-xl flex flex-wrap items-center justify-between mx-auto pl-0">
                 <span className="self-center text-2xl font-semibold whitespace-nowrap underline">devQ</span>
@@ -13,3 +19,5 @@ export default function NavHome() {
         </nav >
     )
 }
+
+export default NavHome;
