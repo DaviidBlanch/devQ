ecdf211c519ab498807e104955d4e11265ea98b6
David Blanch Bellido <dablabe98@gmail.com>
2023-08-12 18:13:31 +0200

    fix import logo

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,7 +1,7 @@
 import { HomeSolidIcon, SearchOutlineIcon } from "../assets/Icons";
 import { Link } from "react-router-dom";
-import CapgeminiLogo from "../assets/Images/CapgeminiLogo.webp";
-import NttDataLogo from "../assets/Images/NttDataLogo.webp";
+import CapgeminiLogo from "../assets/images/CapgeminiLogo.webp";
+import NttDataLogo from "../assets/images/NttDataLogo.webp";
 
 export default function SideBar() {
     return (
