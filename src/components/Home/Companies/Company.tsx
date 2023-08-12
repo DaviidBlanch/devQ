68795bdb77f9bd0a0f1480c2f7a8fd164942567f
David Blanch Bellido <dablabe98@gmail.com>
2023-08-12 18:15:56 +0200

    rename import

--- src/components/Home/Companies/Company.tsx
+++ src/components/Home/Companies/Company.tsx
@@ -1,11 +1,11 @@
-import Capgemini from "../../../assets/images/NttDataLogo.webp"
+import NttData from "../../../assets/images/NttDataLogo.webp"
 
 export default function Company() {
     return (
         <div className="bg-[#181818] p-4 w-full rounded-lg hover:bg-[#252525] cursor-pointer">
             <div className="h-full">
                 <div className="mb-4">
-                    <img src={Capgemini} alt="company" className="object-cover rounded-lg w-full h-full l-0 top-0 shadow-lg shadow-black" />
+                    <img src={NttData} alt="company" className="object-cover rounded-lg w-full h-full l-0 top-0 shadow-lg shadow-black" />
                 </div>
                 <div className="min-h-full">
                     <span className="z-1 inline-block max-w-full align-middle lg:text-lg md:text-lg sm:text-lg text-white font-bold">Capgemini</span>
