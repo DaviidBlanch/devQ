05576dc875ba580c0db7f6b5471377f13469face
David Blanch Bellido <dablabe98@gmail.com>
2023-08-13 12:09:07 +0200

    Fix navbar from companyDetails

--- src/components/Home/Companies/CompanyDetails.tsx
+++ src/components/Home/Companies/CompanyDetails.tsx
@@ -36,23 +36,23 @@ export default function CompanyDetails() {
                     <div>
                         <div className="text-lg text-center text-white">
                             <ul className="lg:flex md:flex border-b border-white lg:w-2/4 md:w-3/4 sm:w-4/5">
-                                <li className="mr-3">
-                                    <a href="#" className="inline-block p-3 pt-0">
+                                <li className="mr-6 mb-1">
+                                    <a href="#" className="hover:bg-[#5757578f] rounded-lg inline-block p-1">
                                         Junior
                                     </a>
                                 </li>
-                                <li className="mr-3">
-                                    <a href="#" className="inline-block p-3 pt-0" aria-current="page">
+                                <li className="mr-6">
+                                    <a href="#" className="hover:bg-[#5757578f] rounded-lg inline-block p-1">
                                         Semi
                                     </a>
                                 </li>
-                                <li className="mr-3">
-                                    <a href="#" className="inline-block p-3 pt-0">
+                                <li className="mr-6">
+                                    <a href="#" className="hover:bg-[#5757578f] rounded-lg inline-block p-1">
                                         Senior
                                     </a>
                                 </li>
-                                <li className="lg:flex md:flex items-center">
-                                    <a href="#" className="inline-block p-3 pt-0">
+                                <li className="lg:flex md:flex items-center sm:mr-6 min-[500px]:mr-6">
+                                    <a href="#" className="hover:bg-[#5757578f] rounded-lg inline-block p-1">
                                         <FilesIcon />
                                     </a>
                                 </li>
--- src/components/Home/NavBar.tsx
+++ src/components/Home/NavBar.tsx
@@ -10,7 +10,7 @@ const NavBar: React.FC<Scroll> = ({ scroll }) => {
             className={`sticky w-auto z-20 top-0 left-0 px-6 py-3 rounded-t-lg transition-colors: duration-1000 ${scroll ? "bg-[#0d363f]" : "bg-transparent"}`}
         >
             <div className=" max-w-screen-xl flex flex-wrap items-center justify-between mx-auto pl-0">
-                <span className="self-center text-2xl font-semibold whitespace-nowrap underline"></span>
+                <span className="self-center text-2xl font-semibold whitespace-nowrap underline">devQ</span>
                 <div>
                     <button type="button" className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300" aria-expanded="false" data-dropdown-toggle="dropdown-user">
                         <img className="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo" />
