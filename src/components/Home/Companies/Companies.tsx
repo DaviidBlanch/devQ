388ded8f17aff17f4d4bc63d8e7ee33a5de5fccd
David Blanch Bellido <dablabe98@gmail.com>
2023-09-10 14:27:55 +0200

    Remove comments

--- src/components/Home/Companies/Companies.tsx
+++ src/components/Home/Companies/Companies.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import Company from "./Company";
 import { Link, useParams } from "react-router-dom";
 import { useFetchCompany } from "../../../hooks/posts";
--- src/components/Home/Companies/CompanyDetails.tsx
+++ src/components/Home/Companies/CompanyDetails.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { useState } from "react";
 import NavBar from "../NavBar";
 import Background from "../Background";
