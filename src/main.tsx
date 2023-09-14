1ee1f80d762420e890ee14b6de5eb5ec6cf22b48
David Blanch Bellido <david.blanch-bellido-external@capgemini.com>
2023-09-14 12:03:29 +0200

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,13 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
-import { SidePanelProvider } from './useContexts.tsx/SidePanelContext.tsx'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
-      <SidePanelProvider>
-        <App />
-      </SidePanelProvider>
+      <App />
     </BrowserRouter>
   </QueryClientProvider>
 )
