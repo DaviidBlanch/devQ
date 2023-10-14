7faeac4138179c66b8835069962e691ba4eeb07c
David Blanch Bellido <dablabe98@gmail.com>
2023-10-14 19:51:45 +0200

    Add Company detail

--- src/components/Home/CompanyDetail/CompanyDetails.tsx
+++ src/components/Home/CompanyDetail/CompanyDetails.tsx
@@ -3,10 +3,9 @@
 import { useState } from "react";
 import NavBar from "../../Header/NavBar";
 import Background from "../Background";
-import InfoCompany from "./InfoComapny";
-import Questions from "./Questions";
 import { useParams } from "react-router-dom";
 import { useFetchCompanyDetail } from "../../../hooks/posts";
+import Questions from "./Questions";
 
 export default function CompanyDetails() {
 
@@ -23,23 +22,56 @@ export default function CompanyDetails() {
     return (
         <Background handleScroll={handleScroll}>
             <NavBar scroll={isScrolled} />
-            <article className="p-4 px-6 pt-3">
-                <div className="grid grid-cols-2 gap-4">
+            {post !== undefined && !isLoading && !isError && (
+                <article>
+                    <div
+                        className="relative flex flex-col h-full bg-zinc-900 overflow-x-hidden"
+                    >
+                        <header className="flex flex-row gap-8 px-6 mt-6">
+                            <picture className="aspect-square w-52 h-52 flex-none">
+                                <img
+                                    src={post.image}
+                                    alt={`Logo de ${post.image}`}
+                                    className="object-contain w-full h-full shadow-lg"
+                                />
+                            </picture>
+                            <div className="flex flex-col justify-between">
+                                <h2 className="flex flex-1 items-end">{post.city}</h2>
+                                <div>
+                                    <h1 className="text-5xl font-bold block text-white">
+                                        {post.company}
+                                    </h1>
+                                </div>
 
-                    {post !== undefined && !isLoading && !isError && (
-                        <>
-                            <InfoCompany image={post.image} company={post.company} description={post.description} city={post.city} />
+                                <div className="flex-1 flex items-end">
+                                    <div className="text-sm text-gray-300 font-normal">
+                                        <div>
+                                            <span>{post.description}</span>
+                                        </div>
+                                        <p className="mt-1">
+                                            <span className="text-white">{post.questions.length === 1 ? `${post.questions.length} pregunta` : `${post.questions.length} preguntas`} - {post.create_date}</span>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </header>
+                        <div className="relative z-10 px-6 pt-10"></div>
+                        <div
+                            className="absolute inset-0 bg-gradient-to-t from-zinc-900 via-zinc-900/80 -z-[1]"
+                        >
+                        </div>
+                        <section className="p-6">
                             <Questions {...post} />
-                        </>
-                    )}
+                        </section>
+                    </div >
+                </article >
+            )}
 
-                    {isLoading && <strong>Cargando...</strong>}
+            {isLoading && <strong>Cargando...</strong>}
 
-                    {isError && <strong>Ha habido un error</strong>}
+            {isError && <strong>Ha habido un error</strong>}
 
-                    {(post.length === 0 || post === undefined) && !isLoading && !isError && <strong>No hay resultados</strong>}
-                </div>
-            </article>
+            {(post.length === 0 || post === undefined) && !isLoading && !isError && <strong>No hay resultados</strong>}
         </Background >
     )
 }
--- src/components/Home/CompanyDetail/Questions.tsx
+++ src/components/Home/CompanyDetail/Questions.tsx
@@ -1,16 +1,39 @@
+import { RedirectIA } from "../../../assets/Icons";
 import { Posts } from "../../interfaces";
-import FilterQuestions from "./FilterQuestions";
 
 export default function Questions(post: Posts) {
-
     return (
-        <section>
-            <FilterQuestions />
-            <ul className="list-decimal lg:text-base md:text-base min-[500px]:text-xs min-[500px]:ml-3 pt-3">
-                {post.questions.map((question, index) => (
-                    <li key={index} className="mb-3 mt-3 font-semibold">{question}</li>
-                ))}
-            </ul>
-        </section>
+        <table className="table-auto text-left min-w-full divide-y divide-gray-500/20">
+            <thead className="">
+                <tr className="text-zinc-400 text-sm">
+                    <th className="px-4 py-2 font-light">#</th>
+                    <th className="px-4 py-2 font-light">Pregunta</th>
+                    <th className="px-4 py-2 font-light">Experiencia</th>
+                    <th className="px-4 py-2 font-light">IA</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr className="h-[16px]"></tr>
+                {
+                    post.questions.map((question, index) => (
+                        <tr
+                            key={post.id}
+                            className="border-spacing-0 text-gray-300 text-sm font-light hover:bg-white/10 overflow-hidden transition duration-300"
+                        >
+                            <td className="px-4 py-2 rounded-tl-lg rounded-bl-lg w-5">{index + 1}</td>
+                            <td className="px-4 py-2 flex gap-3">
+                                <div className="flex flex-col">
+                                    <h3 className="text-white text-base font-normal">{question}</h3>
+                                </div>
+                            </td>
+                            <td className="px-4 py-2">{post.experience}</td>
+                            <td className="px-4 py-2">
+                                <RedirectIA />
+                            </td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </table>
     )
-}
+} 
